fc0bd6012e22a96d5a4e499702043c1899134f1c
Mitesh Shah <miteshninja@gmail.com>
2016-05-01 04:32:41 +0530

    Local config variable fix

--- visa/database.py
+++ visa/database.py
@@ -5,7 +5,7 @@ from visa import app
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+        db = g._database = sqlite3.connect(app.config['DATABASE'])
         db.row_factory = sqlite3.Row
     return db
 
